6493a040c0896bfb53a6c012a7001792f872aa2a
Tazzmanian <banskaliqtabg@yahoo.com>
2017-02-11 20:55:31 +0200

    remove test dir

--- test/src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO user_table (FNAME, LNAME, BIRTHDATE, EMAIL, PHONE) VALUES ('Teodor', 'Todorov', '1987-02-06', 'banskaliqtabg@yahoo.com', '0123456789');
-INSERT INTO user_table (FNAME, LNAME, BIRTHDATE, EMAIL, PHONE) VALUES ('Test', 'Testov', '1999-12-12', 'test@test.com', '0321654978');
